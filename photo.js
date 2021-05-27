2773e6d0c2696b619365376f95e86a7f8d0d4d12
ernestrc <ernest@unstable.build>
2021-05-27 07:45:57 -0700

    Fix photos sort race

--- photo.js
+++ photo.js
@@ -3,13 +3,15 @@ function loadPhotos() { // eslint-disable-line no-unused-vars
 	var api_url = 'https://graph.facebook.com/v10.0/instagram_oembed?access_token=145798694237169|0a3678ae40d59df4563f67a03adbdb50&omitscript=true';
 	var posts = [
 		'https://www.instagram.com/p/COdsaDBDeTx/',
-		'https://www.instagram.com/p/CDMusEhD0YF/',
+		'https://www.instagram.com/p/CEGAFg9jfWV/',
 		'https://www.instagram.com/p/CCUmF9Ojfnz/',
-		'https://www.instagram.com/p/CORDAC0jrI7/',
+		'https://www.instagram.com/p/CDMusEhD0YF/',
 	];
-	var postElements = [];
 
-	function parseNext(response) {
+	var postElements = [];
+	var i = 0;
+
+	function parseResponse(response) {
 		var data = JSON.parse(response);
 		return data.html;
 	}
@@ -22,24 +24,24 @@ function loadPhotos() { // eslint-disable-line no-unused-vars
 	}
 
 
-	function fetchPost(post) {
-		var url = api_url + '&url=' + post;
-		var xhttp = new XMLHttpRequest();
+	function fetchNext() {
+		var post = posts[i];
+		if (++i <= posts.length) {
+			var url = api_url + '&url=' + post;
+			var xhttp = new XMLHttpRequest();
 
-		console.log('loading ' + url);
+			console.log('loading ' + url);
 
-		xhttp.open('GET', url, true);
-		xhttp.onreadystatechange = function() {
-			if (this.readyState == 4 && this.status == 200) {
-				var html = parseNext(xhttp.response);
-				appendLi(photoEl, html)
-			}
-		};
-		xhttp.send();
-	}
-
-	for (var i = 0; i < posts.length; i++) {
-		fetchPost(posts[i]);
+			xhttp.open('GET', url, true);
+			xhttp.onreadystatechange = function() {
+				if (this.readyState == 4 && this.status == 200) {
+					var html = parseResponse(xhttp.response);
+					appendLi(photoEl, html);
+					fetchNext();
+				}
+			};
+			xhttp.send();
+		}
 	}
 
 	var observer = new MutationObserver(function() {
@@ -51,4 +53,6 @@ function loadPhotos() { // eslint-disable-line no-unused-vars
 	});
 	
 	observer.observe(document, {attributes: false, childList: true, characterData: false, subtree:true});
+
+	fetchNext();
 }
